e3251cf4212bc15c9be7cc01a5489fd391ec36ae
momen.hamaveysi <momen.hamaveysi@kubakgroup.com>
2022-01-30 09:55:26 +0330

    clean unused imports

--- app/src/main/java/com/snakyapps/khiardle/ui/Keyboard.kt
+++ app/src/main/java/com/snakyapps/khiardle/ui/Keyboard.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
