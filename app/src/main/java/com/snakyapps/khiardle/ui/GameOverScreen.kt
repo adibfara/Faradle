0d917d4f06fc7384c565cd0f848acdbac3688d64
Adib Faramarzi <adibfara@gmail.com>
2022-02-04 16:03:54 +0330

    Fancier error and win screen

--- app/src/main/java/com/snakyapps/khiardle/ui/GameOverScreen.kt
+++ app/src/main/java/com/snakyapps/khiardle/ui/GameOverScreen.kt
@@ -2,11 +2,15 @@ package com.snakyapps.khiardle.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,6 +18,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.snakyapps.khiardle.backend.viewmodel.GameViewModel
 
@@ -24,18 +31,27 @@ internal fun BoxScope.GameOverScreen(
     shownLost: () -> Unit,
 ) {
 
-    AnimatedVisibility(state.game.isOver, modifier = Modifier.align(Alignment.Center)
+    AnimatedVisibility(state.game.isOver, modifier = Modifier.fillMaxSize(),
+        enter = fadeIn(), exit = fadeOut()
     ) {
+
         Box(Modifier
-            .align(Alignment.BottomCenter)
-            .clip(RoundedCornerShape(5.dp))
-            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxSize()
+            .background(Color(0x7F000000))
             .clickable {
                 shownLost()
             }
-            .padding(84.dp), Alignment.Center) {
-            Text(text = "You lose. Tap to retry.",
-                color = MaterialTheme.colorScheme.onPrimary)
+            .padding(16.dp)
+            .wrapContentHeight()
+            .clip(RoundedCornerShape(5.dp))
+            .background(MaterialTheme.colorScheme.error),
+            Alignment.Center) {
+            Text(text = "You lost. Tap to retry.",
+                color = MaterialTheme.colorScheme.onError,
+                modifier = Modifier.padding(32.dp),
+                style = MaterialTheme.typography.headlineMedium,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Black)
         }
     }
 }
--- app/src/main/java/com/snakyapps/khiardle/ui/WonScreen.kt
+++ app/src/main/java/com/snakyapps/khiardle/ui/WonScreen.kt
@@ -2,11 +2,14 @@ package com.snakyapps.khiardle.ui
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -14,28 +17,40 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.snakyapps.khiardle.backend.viewmodel.GameViewModel
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
-internal fun BoxScope.WonScreen(
+internal fun WonScreen(
     state: GameViewModel.State,
     shownWon: () -> Unit,
 ) {
 
-    AnimatedVisibility(state.game.isWon, modifier = Modifier.align(Alignment.Center)
+    AnimatedVisibility(state.game.isWon, modifier = Modifier.fillMaxSize(),
+        enter = fadeIn(), exit = fadeOut()
     ) {
+
         Box(Modifier
-            .align(Alignment.BottomCenter)
-            .clip(RoundedCornerShape(5.dp))
-            .background(MaterialTheme.colorScheme.primary)
+            .fillMaxSize()
+            .background(Color(0x7F000000))
             .clickable {
                 shownWon()
             }
-            .padding(84.dp), Alignment.Center) {
-            Text(text = "You WON!",
-                color = MaterialTheme.colorScheme.onPrimary)
+            .padding(16.dp)
+            .wrapContentHeight()
+            .clip(RoundedCornerShape(5.dp))
+            .background(MaterialTheme.colorScheme.primary),
+            Alignment.Center) {
+            Text(text = "LEVEL PASSED!",
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.padding(32.dp),
+                style = MaterialTheme.typography.headlineMedium,
+                textAlign = TextAlign.Center,
+                fontWeight = FontWeight.Black)
         }
     }
 }
